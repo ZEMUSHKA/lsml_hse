6f3c2f0334ac60ad416740345b3846b556e67853
Andrey Zimovnov <zimovnov@gmail.com>
2017-02-09 15:54:49 +0300

    fix

--- azure/enlarge_os_disk.py
+++ azure/enlarge_os_disk.py
@@ -5,9 +5,16 @@ from utils import RG_TEMPLATE
 
 STUDENT_NAME = "admin"
 RG_NAME = RG_TEMPLATE.format(STUDENT_NAME)
-VM_NAME = "ubuntugpu"
-DISK_SIZE = 500
 
-utils.deallocate_vm(VM_NAME, RG_NAME)
-utils.resize_os_disk(RG_NAME, VM_NAME, DISK_SIZE)
-utils.start_vm(VM_NAME, RG_NAME)
+# use comments
+settings = [
+    ["cluster1", 500],
+    ["cluster2", 500],
+    ["cluster3", 500],
+    ["ubuntugpu", 1000]
+]
+
+for VM_NAME, DISK_SIZE in settings:
+    utils.deallocate_vm(VM_NAME, RG_NAME)
+    utils.resize_os_disk(RG_NAME, VM_NAME, DISK_SIZE)
+    utils.start_vm(VM_NAME, RG_NAME)
