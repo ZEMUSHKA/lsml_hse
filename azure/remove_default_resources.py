500c16d6d660621017295bc2e5961742cd3979a8
Andrey Zimovnov <zimovnov@gmail.com>
2017-02-17 14:48:44 +0300

    remove all user resources

---
+++ azure/remove_default_resources.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import subprocess
+from utils import RG_TEMPLATE
+
+users = pd.read_json("users.json", orient="records")
+
+for idx, (_, row) in enumerate(users.iterrows()):
+    # if idx < 10:
+    #     continue
+    row = dict(row)
+    user = row["user"]
+    resGrName = RG_TEMPLATE.format(user)
+    # create res gr
+    subprocess.check_output(
+        """
+        az group delete \
+        -n "{n}"
+        """.format(n=resGrName),
+        shell=True
+    )
+    print user, "done"
