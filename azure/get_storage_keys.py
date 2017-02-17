20496eef96e56732a49f1fff9f0a31cb8e2d4d25
Andrey Zimovnov <zimovnov@gmail.com>
2017-02-17 17:48:07 +0300

    storage keys

---
+++ azure/get_storage_keys.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+from utils import RG_TEMPLATE, STORAGE_ACCOUNT_TEMPLATE
+from utils import get_storage_key
+import json
+
+users = pd.read_json("users.json", orient="records")
+
+storage_keys = dict()
+storage_keys["admin"] = get_storage_key(STORAGE_ACCOUNT_TEMPLATE.format("admin"), RG_TEMPLATE.format("admin"))
+
+for _, row in users.iterrows():
+    row = dict(row)
+    user = row["user"]
+    user_account = STORAGE_ACCOUNT_TEMPLATE.format(user)
+    user_rg = RG_TEMPLATE.format(user)
+    user_key = get_storage_key(user_account, user_rg)
+    storage_keys[user] = user_key
+    print user, "done"
+
+json.dump(storage_keys, open("storage_keys.json", "w"), indent=4, sort_keys=True)
