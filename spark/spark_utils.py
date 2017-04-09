50481e1cc85eca83756ef0fbd4dab2c08771e507
Andrey Zimovnov <zimovnov@gmail.com>
2017-04-09 21:27:35 +0300

    refactoring

--- spark/spark_utils.py
+++ spark/spark_utils.py
@@ -1,26 +1,34 @@
 import pyspark
 
 
-def get_spark_conf(CORES_PER_EXECUTOR=1, NODES_ALIVE=3):
-    # optimal EXECUTOR_CORES is factor of 4, spark.executor.instances is ignored if > 1
-    EXECUTOR_INSTANCES = int(NODES_ALIVE * 4 / CORES_PER_EXECUTOR) - 1  # One for Application Master
-    EXECUTOR_MEMORY_MB = 6144 * CORES_PER_EXECUTOR - max(384, int(6144 * CORES_PER_EXECUTOR * 0.10) + 1)
-    SPARK_PARALLELISM = EXECUTOR_INSTANCES * CORES_PER_EXECUTOR
-    if SPARK_PARALLELISM < 100:
-        SPARK_PARALLELISM = 100
-    conf = pyspark.SparkConf() \
-        .set("spark.executor.cores", CORES_PER_EXECUTOR) \
-        .set("spark.executor.memory", "{0}m".format(EXECUTOR_MEMORY_MB)) \
-        .set("spark.executor.instances", EXECUTOR_INSTANCES) \
-        .set("spark.default.parallelism", SPARK_PARALLELISM)
+def get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive,
+                   executorsPerNode, memoryPerExecutor):
+    executorInstances = nodesAlive * executorsPerNode - 1  # One for Application Master
+    executorMemoryOverheadMb = max(384, int(memoryPerExecutor * 0.10) + 1)  # default Spark behavior
+    if addPythonMemoryOverhead:
+        # python eats the same amount, add to overhead!
+        executorMemoryOverheadMb = int(memoryPerExecutor * 0.5)
+    executorMemoryMb = memoryPerExecutor - executorMemoryOverheadMb
+    conf = (
+        pyspark.SparkConf()
+        .set("spark.executor.memory", "{0}m".format(executorMemoryMb))
+        .set("spark.driver.memory", "{0}m".format(executorMemoryMb))
+        .set("spark.yarn.executor.memoryOverhead", executorMemoryOverheadMb)
+        .set("spark.yarn.driver.memoryOverhead", executorMemoryOverheadMb)
+        .set("spark.python.worker.memory", executorMemoryOverheadMb * 0.8)  # 10 % of memory is for other stuff
+        .set("spark.executor.instances", executorInstances)
+        .set("spark.default.parallelism", parallelism)
+    )
     return conf
 
 
-def get_spark_context(master="yarn-client", appName="Jupyter Notebook", CORES_PER_EXECUTOR=1, NODES_ALIVE=3):
+def get_spark_context(master="yarn-client", appName="Jupyter Notebook",
+                      parallelism=300, addPythonMemoryOverhead=True, nodesAlive=3,
+                      executorsPerNode=4, memoryPerExecutor=6144):
     sc = pyspark.SparkContext(
         master=master,
         appName=appName,
-        conf=get_spark_conf(CORES_PER_EXECUTOR, NODES_ALIVE)
+        conf=get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive, executorsPerNode, memoryPerExecutor)
     )
     print "Ambari - http://10.0.1.21:8080"
     print "All Applications - http://10.0.1.23:8088/cluster"
