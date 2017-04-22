90c669833cb973cb85c7de1c4db86dba46939d79
Andrey Zimovnov <zimovnov@gmail.com>
2017-04-22 20:40:06 +0300

    spark more conf

--- spark/spark_utils.py
+++ spark/spark_utils.py
@@ -2,7 +2,7 @@ import pyspark
 
 
 def get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive,
-                   executorsPerNode, memoryPerExecutor):
+                   executorsPerNode, memoryPerExecutor, additionalConf):
     executorInstances = nodesAlive * executorsPerNode - 1  # One for Application Master
     executorMemoryOverheadMb = 1024
     pythonMemoryOverheadMb = 0
@@ -20,16 +20,19 @@ def get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive,
         .set("spark.executor.instances", executorInstances)
         .set("spark.default.parallelism", parallelism)
     )
+    for k, v in additionalConf.iteritems():
+        conf = conf.set(k, v)
     return conf
 
 
 def get_spark_context(master="yarn-client", appName="Jupyter Notebook",
                       parallelism=300, addPythonMemoryOverhead=True, nodesAlive=3,
-                      executorsPerNode=4, memoryPerExecutor=6144):
+                      executorsPerNode=4, memoryPerExecutor=6144, additionalConf={}):
     sc = pyspark.SparkContext(
         master=master,
         appName=appName,
-        conf=get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive, executorsPerNode, memoryPerExecutor)
+        conf=get_spark_conf(parallelism, addPythonMemoryOverhead, nodesAlive,
+                            executorsPerNode, memoryPerExecutor, additionalConf)
     )
     print "Ambari - http://10.0.1.21:8080"
     print "All Applications - http://10.0.1.23:8088/cluster"
