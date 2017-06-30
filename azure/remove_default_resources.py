9bb960539e3062da30411917815f58d08d886c94
Andrey Zimovnov <zimovnov@gmail.com>
2017-06-30 11:13:47 +0300

    remove all but teachers

--- azure/remove_default_resources.py
+++ azure/remove_default_resources.py
@@ -7,8 +7,8 @@ from utils import RG_TEMPLATE
 users = pd.read_json("users.json", orient="records")
 
 for idx, (_, row) in enumerate(users.iterrows()):
-    # if idx < 10:
-    #     continue
+    if idx < 3:
+        continue
     row = dict(row)
     user = row["user"]
     resGrName = RG_TEMPLATE.format(user)
