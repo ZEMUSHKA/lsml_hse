bd65fc584c4dfa7fe0eadf92b6132a53e940fb13
Andrey Zimovnov <zimovnov@gmail.com>
2018-03-23 19:27:17 +0300

    fix and lowercase

--- azure/create_ubuntu_gpu.py
+++ azure/create_ubuntu_gpu.py
@@ -13,16 +13,16 @@ parser.add_argument("--create_shared", action="store_true", help="create shared 
 parser.add_argument("--create_aux", action="store_true", help="create aux resources, only once per script run")
 args = parser.parse_args()
 
-STUDENT_NAME = args.user
-RG_NAME = RG_TEMPLATE.format(STUDENT_NAME)
-STORAGE_ACCOUNT = STORAGE_ACCOUNT_TEMPLATE.format(STUDENT_NAME)
-region = region_by_user[STUDENT_NAME]
+student_name = args.user
+rg_name = RG_TEMPLATE.format(student_name)
+storage_account = STORAGE_ACCOUNT_TEMPLATE.format(student_name)
+region = region_by_user[student_name]
 
 RESIZE_OS_DISK = False
 OS_DISK_SIZE = 1023
 
 if args.create_shared:
-    utils.create_shared(RG_NAME, region)
+    utils.create_shared(rg_name, region)
 
 IP_NAME = "ip_ubuntugpu"
 NIC_NAME = "nic_ubuntugpu"
@@ -32,15 +32,15 @@ IP = "10.0.1.10"
 
 if args.create_aux:
     # create public IP
-    utils.create_public_ip(IP_NAME, RG_NAME)
+    utils.create_public_ip(IP_NAME, rg_name)
 
     # Create network card with fixed private IP
-    utils.create_nic_with_private_ip(NIC_NAME, RG_NAME, VNET_NAME, SUBNET_NAME, NSG_NAME, IP_NAME, INT_DNS_NAME, IP)
+    utils.create_nic_with_private_ip(NIC_NAME, rg_name, VNET_NAME, SUBNET_NAME, NSG_NAME, IP_NAME, INT_DNS_NAME, IP)
 
 # create VM
 VM_NAME = INT_DNS_NAME
-VM_SIZE = gpus_by_user[STUDENT_NAME]
-PUB_KEY = args.ssh_key
+vm_size = gpus_by_user[student_name]
+pub_key = args.ssh_key
 
 IMAGE_NAME = "/subscriptions/" + utils.get_subscription_id() + \
              "/resourceGroups/admin_resources/providers/Microsoft.Compute/images/ubuntu_gpu_image1_" + region
@@ -54,13 +54,13 @@ with open(cloud_init_fn, "w") as f:
         open("configs/cloud_init_ubuntugpu_template.txt").read().replace("###PASSWORD###", user_pass)
     )
 
-utils.create_vm(VM_NAME, RG_NAME, region, IMAGE_NAME, NIC_NAME, VM_SIZE, PUB_KEY, OS_DISK_NAME,
-                cloud_init_fn, data_disks)
+utils.create_vm(VM_NAME, rg_name, region, IMAGE_NAME, NIC_NAME, vm_size, pub_key, OS_DISK_NAME,
+                user_pass, cloud_init_fn, data_disks)
 
 if RESIZE_OS_DISK:
-    utils.deallocate_vm(VM_NAME, RG_NAME)
-    utils.resize_managed_disk(RG_NAME, OS_DISK_NAME, OS_DISK_SIZE)
-    utils.start_vm(VM_NAME, RG_NAME)
+    utils.deallocate_vm(VM_NAME, rg_name)
+    utils.resize_managed_disk(rg_name, OS_DISK_NAME, OS_DISK_SIZE)
+    utils.start_vm(VM_NAME, rg_name)
 
-print "ubuntugpu public IP: {}".format(utils.get_public_ip("ip_ubuntugpu", RG_NAME))
+print "ubuntugpu public IP: {}".format(utils.get_public_ip("ip_ubuntugpu", rg_name))
 print "password:", user_pass
