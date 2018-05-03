bf8c1bf7369d0825f2d5cefda98f4860c65401bf
Andrey Zimovnov <zimovnov@gmail.com>
2018-05-03 14:37:42 +0300

    new subscription

--- azure/create_images_in_regions.py
+++ azure/create_images_in_regions.py
@@ -26,8 +26,8 @@ def create_image(RG_NAME, IMAGE_NAME, SOURCE, REGION):
 def create_image_lock(RG_NAME, IMAGE_NAME):
     check_output_wrapper(
         """
-        az lock create -t CanNotDelete -n lock -g {RG_NAME} --parent-resource-path "" --resource-name "{IMAGE_NAME}" \
-        --resource-provider-namespace "" --resource-type "Microsoft.Compute/images"
+        az lock create -t CanNotDelete -n lock -g {RG_NAME} --resource-name "{IMAGE_NAME}" \
+        --resource-type "Microsoft.Compute/images"
         """.format(**locals()),
         shell=True
     )
@@ -50,10 +50,10 @@ def assign_role_to_student_group(IMAGE_NAME):
 for region in ["eastus", "southcentralus", "westeurope", "southeastasia"]:
     create_image("admin_resources",
                  "ubuntu_gpu_image1_{0}".format(region),
-                 "https://lsml1{0}.blob.core.windows.net/images/ubuntugpu.vhd".format(region),
+                 "https://lsml3{0}.blob.core.windows.net/images/ubuntugpu.vhd".format(region),
                  region)
     for clIdx in [1, 2, 3]:
         create_image("admin_resources",
                      "cluster{1}_image1_{0}".format(region, clIdx),
-                     "https://lsml1{0}.blob.core.windows.net/images/cluster{1}.vhd".format(region, clIdx),
+                     "https://lsml3{0}.blob.core.windows.net/images/cluster{1}.vhd".format(region, clIdx),
                      region)
