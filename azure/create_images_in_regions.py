be3310ee07c3c6db5bcc19c22ebcbf62f29cf02a
Andrey Zimovnov <zimovnov@gmail.com>
2018-05-09 13:46:32 +0300

    back to original subscription

--- azure/create_images_in_regions.py
+++ azure/create_images_in_regions.py
@@ -50,10 +50,10 @@ def assign_role_to_student_group(IMAGE_NAME):
 for region in ["eastus", "southcentralus", "westeurope", "southeastasia"]:
     create_image("admin_resources",
                  "ubuntu_gpu_image1_{0}".format(region),
-                 "https://lsml3{0}.blob.core.windows.net/images/ubuntugpu.vhd".format(region),
+                 "https://lsml1{0}.blob.core.windows.net/images/ubuntugpu.vhd".format(region),
                  region)
     for clIdx in [1, 2, 3]:
         create_image("admin_resources",
                      "cluster{1}_image1_{0}".format(region, clIdx),
-                     "https://lsml3{0}.blob.core.windows.net/images/cluster{1}.vhd".format(region, clIdx),
+                     "https://lsml1{0}.blob.core.windows.net/images/cluster{1}.vhd".format(region, clIdx),
                      region)
