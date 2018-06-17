8209ef9f37639e935e80ec543ce9a540dae3c6d5
Andrey Zimovnov <zimovnov@gmail.com>
2018-06-18 01:07:13 +0300

    report for fintech

--- azure/check_cpu_usage.py
+++ azure/check_cpu_usage.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import subprocess
+import json
 
 import pandas as pd
 
 import utils
 
-users = pd.read_json("users.json", orient="records")
+# users = pd.read_json("users.json", orient="records")
+
+users = json.load(open("sber.json"))
+users = pd.DataFrame(users.keys(), columns=["user"])
 
 subscription_id = utils.get_subscription_id()
 VM_NAME = "cluster1"
@@ -20,6 +23,7 @@ for idx, (_, row) in enumerate(users.iterrows()):
     row = dict(row)
     user = row["user"]
     rg_name = utils.get_student_resource_group(user)
+    print user, rg_name
     out = utils.get_cpu_usage(subscription_id, rg_name, VM_NAME, start, end, interval)
     if out is not None:
         out = dict(out)
@@ -28,4 +32,4 @@ for idx, (_, row) in enumerate(users.iterrows()):
 
 df = pd.DataFrame(df)
 df = df[sorted(df.columns)]
-df.to_excel("cpu_report.xlsx")
+df.to_excel("cpu_report1_fintech.xlsx")
