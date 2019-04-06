6975d3e80ae1c78af88999e725506dfd1037c18e
Andrey Zimovnov <zimovnov@gmail.com>
2019-04-07 02:23:34 +0300

    cleaner

--- azure/create_cluster.py
+++ azure/create_cluster.py
@@ -5,26 +5,22 @@ import argparse
 from joblib import Parallel, delayed
 
 import utils
-from utils import VNET_NAME, SUBNET_NAME, NSG_NAME, cloud_init_fill_template, CLUSTER_IMAGE
+from utils import VNET_NAME, SUBNET_NAME, NSG_NAME, cloud_init_fill_template, CLUSTER_IMAGE, RG_NAME, REGION
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--resource_group", action="store", default="my_resources")
-parser.add_argument("--region", action="store", default="eastus")
 parser.add_argument("--create_shared", action="store_true", help="create shared resources")
 parser.add_argument("--create_aux", action="store_true", help="create aux resources, only once per script run")
 parser.add_argument("--jobs", type=int, action="store", help="number of parallel jobs")
 args = parser.parse_args()
 
 
-rg_name = args.resource_group
-region = args.region
 VM_SIZE = "Standard_E4_v3"
 
 RESIZE_OS_DISK = False
 OS_DISK_SIZE = 511
 
 if args.create_shared:
-    utils.create_shared(rg_name, region)
+    utils.create_shared(RG_NAME, REGION)
 
 
 def create_cluster_node(idx, user_pass):
@@ -41,14 +37,14 @@ def create_cluster_node(idx, user_pass):
     if args.create_aux:
         # create public IP
         if IP_NAME is not None:
-            utils.create_public_ip(IP_NAME, rg_name)
+            utils.create_public_ip(IP_NAME, RG_NAME)
 
         # Create network card with fixed private IP
-        utils.create_nic_with_private_ip(NIC_NAME, rg_name, VNET_NAME, SUBNET_NAME, NSG_NAME, IP_NAME, INT_DNS_NAME, IP)
+        utils.create_nic_with_private_ip(NIC_NAME, RG_NAME, VNET_NAME, SUBNET_NAME, NSG_NAME, IP_NAME, INT_DNS_NAME, IP)
 
     # create VM https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-sizes
     IMAGE_NAME = "/subscriptions/" + utils.get_subscription_id() + \
-                 "/resourceGroups/" + args.resource_group + "/providers/Microsoft.Compute/images/" + \
+                 "/resourceGroups/" + RG_NAME + "/providers/Microsoft.Compute/images/" + \
                  CLUSTER_IMAGE.format(idx)
     data_disks = "255 255 255 255"
 
@@ -56,13 +52,13 @@ def create_cluster_node(idx, user_pass):
         cloud_init_fn = cloud_init_fill_template("configs/cloud_init_cluster_master_template.txt", user_pass)
     else:
         cloud_init_fn = "configs/cloud_init_cluster_slave.txt"
-    utils.create_vm(VM_NAME, rg_name, region, IMAGE_NAME, NIC_NAME, VM_SIZE, None, OS_DISK_NAME,
+    utils.create_vm(VM_NAME, RG_NAME, REGION, IMAGE_NAME, NIC_NAME, VM_SIZE, None, OS_DISK_NAME,
                     user_pass, cloud_init_fn, data_disks, "Standard_LRS")
 
     if RESIZE_OS_DISK:
-        utils.deallocate_vm(VM_NAME, rg_name)
-        utils.resize_managed_disk(rg_name, OS_DISK_NAME, OS_DISK_SIZE)
-        utils.start_vm(VM_NAME, rg_name)
+        utils.deallocate_vm(VM_NAME, RG_NAME)
+        utils.resize_managed_disk(RG_NAME, OS_DISK_NAME, OS_DISK_SIZE)
+        utils.start_vm(VM_NAME, RG_NAME)
 
 
 user_pass = utils.generate_pass()
@@ -70,5 +66,5 @@ Parallel(n_jobs=args.jobs or 3, backend="threading")(
     delayed(create_cluster_node)(idx, user_pass) for idx in [1, 2, 3]
 )
 
-print("cluster1 public IP: {}".format(utils.get_public_ip("ip_cluster1", rg_name)))
+print("cluster1 public IP: {}".format(utils.get_public_ip("ip_cluster1", RG_NAME)))
 print("password:", user_pass)
