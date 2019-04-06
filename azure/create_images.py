d418311c939508aa87eca82c6deaa733791ce373
Andrey Zimovnov <zimovnov@gmail.com>
2019-04-07 02:21:44 +0300

    cleaner

--- azure/create_images.py
+++ azure/create_images.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 import argparse
 
-from utils import timeit, check_output_wrapper, CLUSTER_IMAGE, UBUNTUGPU_IMAGE
+from utils import timeit, check_output_wrapper, CLUSTER_IMAGE, UBUNTUGPU_IMAGE, REGION, RG_NAME
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--storage_account", action="store", required=True)
-parser.add_argument("--resource_group", action="store", default="my_resources")
-parser.add_argument("--region", action="store", default="eastus")
 args = parser.parse_args()
 
 
@@ -26,12 +24,12 @@ def create_image(RG_NAME, IMAGE_NAME, SOURCE, REGION):
     )
 
 
-create_image(args.resource_group,
+create_image(RG_NAME,
              UBUNTUGPU_IMAGE,
              "https://{0}.blob.core.windows.net/images/ubuntugpu.vhd".format(args.storage_account),
-             args.region)
+             REGION)
 for clIdx in [1, 2, 3]:
-    create_image(args.resource_group,
+    create_image(RG_NAME,
                  CLUSTER_IMAGE.format(clIdx),
                  "https://{0}.blob.core.windows.net/images/cluster{1}.vhd".format(args.storage_account, clIdx),
-                 args.region)
+                 REGION)
